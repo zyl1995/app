f7c5b9c6a548b055ce4d49f909a89a5f9fb88d67
lin <18850586671@163.com>
2019-03-11 23:30:09 +0800

    fix fd not close issue

--- async.py
+++ async.py
@@ -6,7 +6,7 @@ HOST, PORT = '', 9999
 listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 listen_socket.bind((HOST, PORT))
-listen_socket.listen(5)
+listen_socket.listen(100)
 # socket设置为非阻塞
 listen_socket.setblocking(0)
 # 创建一个epoll对象
@@ -26,6 +26,7 @@ try:
             print('not event, continue')
             continue
         for fd, event in events:
+            print(fd, event)
             # 说明有新连接到来
             if fd == listen_socket.fileno():
                 connection, addr = listen_socket.accept()
@@ -35,32 +36,37 @@ try:
                 epoll.register(connection.fileno(), select.EPOLLIN)
                 connections[connection.fileno()] = connection
                 requests[connection.fileno()] = b''
+                responses[connection.fileno()] = b'test'
             # 触发读事件
             elif event & select.EPOLLIN:
                 requests[fd] += connections[fd].recv(1024)
                 # 无数据，可能是客户端已关闭
                 if not requests[fd]:
+                    print('------>')
                     connections[fd].close()
                     del requests[fd]
                     del connections[fd]
                     # 不再监听该文件描述符
                     epoll.modify(fd, 0)
                 else:
+                    print(requests[fd])
                     epoll.modify(fd, select.EPOLLOUT)
             # 触发写事件
             elif event & select.EPOLLOUT:
                 byteswritten = connections[fd].send(responses[fd])
-                responses[fd] = responses[byteswritten:]
+                responses[fd] = responses[fd][byteswritten:]
+                print('---->', len(responses[fd]))
                 if len(responses[fd]) == 0:
-                    epoll.modify(fd, 0)
-                    epoll.modify(fd, select.EPOLLIN)
+                    epoll.unregister(fd)
+                    connections[fd].close()
+                    del connections[fd]
             elif event & select.EPOLLHUP:
+                epoll.unregister(fd)
                 connections[fd].close()
                 del connections[fd]
                 del requests[fd]
                 del responses[fd]
 
-
-
 finally:
-    pass
+    epoll.unregister(listen_socket.fileno())
+    listen_socket.close()
